970153ee31d70a80ff35114058e697120260becc
JandoBR <jose.ballestas.rodriguez@gmail.com>
2026-05-04 11:19:32 -0500

    Simplified db and added postgres implementation to jwt

--- api/src/auth/infrastructure/persistence/user.repository.ts
+++ api/src/auth/infrastructure/persistence/user.repository.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../shared/infrastructure/prisma/prisma.service'; 
 import type { IUserAuthRepository } from '../../domain/repositories/user-auth.repository';
 import type { AuthPayload } from '../../domain/entities/auth-payload.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserRepository implements IUserAuthRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
   async validateCredentials(email: string, pass: string): Promise<AuthPayload | null> {
-    // Simulamos una base de datos con un usuario de prueba
-    const MOCK_USER = {
-      email: 'admin@test.com',
-      password: 'password123',
-      data: { sub: 1, email: 'admin@test.com', role: 'ADMIN' }
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) return null;
+
+    const isMatch = await bcrypt.compare(pass, user.password);
+    
+    if (!isMatch) return null;
+
+    return {
+      sub: user.id,        
+      email: user.email,
+      role: user.role, 
     };
-
-    if (email === MOCK_USER.email && pass === MOCK_USER.password) {
-      return MOCK_USER.data;
-    }
-
-    return null;
   }
 }
