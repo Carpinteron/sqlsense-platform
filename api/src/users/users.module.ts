c04deb70552d3a19d9875ee66fd44ff18ac34993
Canela <carpinteron@uninorte.edu.co>
2026-05-07 23:51:00 -0500

    feat(users): implement create and update use cases

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../shared/infrastructure/prisma/prisma.module';
+import { CreateUserUseCase } from './application/use-cases/create-user.use-case';
+import { UpdateUserUseCase } from './application/use-cases/update-user.use-case';
+import { GetAllUsersUseCase } from './application/use-cases/get-all-users.use-case';
+import { GetUserByIdUseCase } from './application/use-cases/get-user-by-id.use-case';
+import { UserRepository } from '../auth/infrastructure/persistence/user.repository';
 
 @Module({
   imports: [PrismaModule],
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [
+    CreateUserUseCase,
+    UpdateUserUseCase,
+    GetAllUsersUseCase,
+    GetUserByIdUseCase,
+    // Bind the repository token to the existing implementation
+    {
+      provide: 'IUserManagementRepository',
+      useClass: UserRepository,
+    },
+  ],
+  exports: [CreateUserUseCase, UpdateUserUseCase, GetAllUsersUseCase, GetUserByIdUseCase],
 })
 export class UsersModule {}
