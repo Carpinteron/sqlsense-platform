83343cb6c4a609476507b09c296684aa33d07d28
Canela <carpinteron@uninorte.edu.co>
2026-05-08 04:10:59 -0500

    docs(users): add swagger decorators and DTO schemas

--- api/src/users/application/dtos/update-user.dto.ts
+++ api/src/users/application/dtos/update-user.dto.ts
@@ -1,10 +1,13 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class UpdateUserDto {
+  @ApiPropertyOptional({ example: 'actualizado@sqlsense.com' })
   @IsOptional()
   @IsEmail()
   email?: string;
 
+  @ApiPropertyOptional({ example: 'nuevaPassword1', minLength: 6 })
   @IsOptional()
   @IsString()
   @MinLength(6)
