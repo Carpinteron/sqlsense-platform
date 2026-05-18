47bcfb485f020a2c179242a56f4e61bd680fbde3
JandoBR <jose.ballestas.rodriguez@gmail.com>
2026-05-18 15:49:32 -0500

    added bullmq dto

--- worker/src/evaluation/infrastructure/processors/submission.processor.ts
+++ worker/src/evaluation/infrastructure/processors/submission.processor.ts
@@ -1,6 +1,9 @@
 import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
+import { plainToInstance } from 'class-transformer';
+import { validateOrReject } from 'class-validator';
 import { EvaluateSubmissionUseCase } from '../../application/use-cases/evaluate-submission.use-case';
+import { EvaluateSubmissionJobDto } from '../../application/dtos/submission-job.dto';
 
 @Processor('submission-queue')
 export class SubmissionProcessor extends WorkerHost {
@@ -10,19 +13,26 @@ export class SubmissionProcessor extends WorkerHost {
     super();
   }
 
-  async process(job: Job<{ submissionId: string }>): Promise<void> {
-    const { submissionId } = job.data;
+  async process(job: Job<unknown>): Promise<void> {
+    const jobData = plainToInstance(EvaluateSubmissionJobDto, job.data);
 
+    try {
+      await validateOrReject(jobData);
+    } catch (errors) {
+      console.error(`[Worker] Estructura del Job inválida para el ID ${job.id}:`, errors);
+      throw new Error('El payload del Job no cumple con los requisitos de validación.');
+    }
+
+    const { submissionId } = jobData;
     console.log(`[Worker] Iniciando procesamiento de la submission: ${submissionId}`);
 
     try {
       await this._evaluateSubmissionUseCase.execute(submissionId);
-      
       console.log(`[Worker] Finalizado con éxito: ${submissionId}`);
-        } catch (error) {
-        const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
-        console.error(`[Worker] Falló la evaluación de ${submissionId}:`, errorMessage);
-        throw error; 
-        }
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
+      console.error(`[Worker] Falló la evaluación de ${submissionId}:`, errorMessage);
+      throw error;
+    }
   }
 }
